15ef5bb0e57cdb37d6451a21b8f02280d85e754e
Anushree Dixit <anushreedixitjyoti@gmail.com>
2026-07-11 10:53:40 +0530

    Create CountryController.java

---
+++ Week3/1. Spring REST using Spring Boot 3/2. spring-rest-handson/Exercise3-GetCountryByCountryCode/CountryController.java
@@ -0,0 +1,24 @@
+package com.cognizant.springlearn.controller;
+
+import com.cognizant.springlearn.model.Country;
+import com.cognizant.springlearn.service.CountryService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/country")
+public class CountryController {
+
+    private final CountryService countryService;
+
+    public CountryController(CountryService countryService) {
+        this.countryService = countryService;
+    }
+
+    @GetMapping("/{code}")
+    public Country getCountry(@PathVariable String code) {
+        return countryService.getCountry(code);
+    }
+}
