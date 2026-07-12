8a57e33a25309113c7684108084c1d7365c9f7a8
Anushree Dixit <anushreedixitjyoti@gmail.com>
2026-07-12 16:37:08 +0530

    Create database.sql

---
+++ Week2/2. Spring Data JPA with Spring Boot, Hibernate/Spring Data JPA – Quick Example/database.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE IF NOT EXISTS ormlearn;
+
+USE ormlearn;
+
+CREATE TABLE IF NOT EXISTS country (
+    co_code VARCHAR(2) PRIMARY KEY,
+    co_name VARCHAR(50)
+);
+
+INSERT INTO country (co_code, co_name)
+VALUES ('IN', 'India');
+
+INSERT INTO country (co_code, co_name)
+VALUES ('US', 'United States of America');
