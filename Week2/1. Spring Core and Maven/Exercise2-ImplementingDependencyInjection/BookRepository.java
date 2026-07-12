0a09d39f8d4005e4d97807e361cea2188b1e3a00
Anushree Dixit <anushreedixitjyoti@gmail.com>
2026-07-12 15:57:29 +0530

    Create BookRepository.java

---
+++ Week2/1. Spring Core and Maven/Exercise2-ImplementingDependencyInjection/BookRepository.java
@@ -0,0 +1,8 @@
+package com.cognizant.library;
+
+public class BookRepository {
+
+    public void displayRepository() {
+        System.out.println("Book Repository Initialized Successfully");
+    }
+}
