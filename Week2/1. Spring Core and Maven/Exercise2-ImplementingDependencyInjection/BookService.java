50572b69f1b219de4ba142e6ffa16192dffd0880
Anushree Dixit <anushreedixitjyoti@gmail.com>
2026-07-12 15:57:43 +0530

    Create BookService.java

---
+++ Week2/1. Spring Core and Maven/Exercise2-ImplementingDependencyInjection/BookService.java
@@ -0,0 +1,14 @@
+package com.cognizant.library;
+
+public class BookService {
+
+    private BookRepository bookRepository;
+
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void displayService() {
+        bookRepository.displayRepository();
+    }
+}
